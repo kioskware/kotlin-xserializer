b14b993937dc586d39c0e715d9d19321f0eb2ffe
FivesoftCode <jan.rozenbajgier@gmail.com>
2025-06-28 14:18:06 +0200

    added forceOptional parameter to customize validation behavior

--- src/main/kotlin/kioskware/xserializer/PropertySerialInfo.kt
+++ src/main/kotlin/kioskware/xserializer/PropertySerialInfo.kt
@@ -1,10 +1,32 @@
 package kioskware.xserializer
 
-// Data class to hold detailed property information as specified
+import kotlinx.serialization.descriptors.SerialDescriptor
+
+/**
+ * Data class representing serialization information for a property.
+ *
+ * @property serialName The name of the property as it will be serialized.
+ * @property isNullable Indicates whether the property can be null.
+ * @property isOptional Indicates whether the property is optional in the serialization context.
+ * @property propertyAnnotations A list of annotations applied to the property itself.
+ * @property classAnnotations A list of annotations applied to the class of the property type.
+ * @property serialDescriptor The [SerialDescriptor] for the property, providing metadata about its type and structure.
+ */
 data class PropertySerialInfo(
     val serialName: String,
     val isNullable: Boolean,
     val isOptional: Boolean,
     val propertyAnnotations: List<Annotation>, // parentDescriptor.getElementAnnotations(index)
-    val classAnnotations: List<Annotation>     // descriptor.annotations
-)
+    val classAnnotations: List<Annotation>,     // descriptor.annotations
+    val serialDescriptor: SerialDescriptor // parentDescriptor.getElementDescriptor(index)
+) {
+
+    /**
+     * A lazy list of all annotations, combining both property and class annotations
+     * for easier access.
+     */
+    val annotations: List<Annotation> by lazy {
+        propertyAnnotations + classAnnotations
+    }
+
+}
